60779ac57a5611ff80f6658d51e8156379940576
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-11 12:59:37 +0600

    update  appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,41 @@
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 //main function start
-main()
-{
-runApp(Myapp());
+main() {
+  runApp(Myapp());
 }
+
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:' MY APP' ,
-      home:Home(),
+      title: ' MY APP',
+      home: Home(),
     );
   }
 }
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:Text('HomePage'),
-      //  centerTitle: true,
-        backgroundColor:Colors.blueAccent,
+      appBar: AppBar(
+        title: Text(
+          'HomePage',
+          style: TextStyle(),
+        ),
+        //  centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Text(
+        'Miel Mahmud Sifat',
+        style: TextStyle(fontSize: 40, color: Colors.amberAccent,fontWeight:FontWeight.bold,),
+
       ),
     );
   }
 }
-
