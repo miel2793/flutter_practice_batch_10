4d4b1a7aa3bb7aaed06efd4aa59cdbf9740ea2a3
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-11 12:31:17 +0600

    update note

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import'package:flutter/material.dart';
 main()
 {
-runApp(app)
+runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
